de151b39504caf6ca5f5ca7aefe4ad79cac01602
vladd23 <vladlungu02@gmail.com>
2023-11-09 21:12:31 +0200

    inceput de login, signup

--- src/main/java/com/voluteamhub/backend/controller/VolunteerController.java
+++ src/main/java/com/voluteamhub/backend/controller/VolunteerController.java
@@ -2,10 +2,15 @@ package com.voluteamhub.backend.controller;
 
 import com.voluteamhub.backend.model.Volunteer;
 import com.voluteamhub.backend.repository.VolunteerRepository;
+import com.voluteamhub.backend.validator.LoginValidator;
+import org.apache.juli.logging.Log;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class VolunteerController {
@@ -17,6 +22,24 @@ public class VolunteerController {
         return volunteerRepository.save(newVolunteer);
     }
 
+    @GetMapping("/getvolunteer")
+    List<Volunteer> getVolunteers(){
+        return volunteerRepository.findAll();
+    }
+
+    @GetMapping("/login")
+    void loginUser(Volunteer v){
+        LoginIn(v);
+    }
 
 
+    public void LoginIn(Volunteer v){
+        List<Volunteer> listaVoluntari = volunteerRepository.findAll();
+
+
+        for(Volunteer volunteer: listaVoluntari){
+            LoginValidator lv = new LoginValidator(volunteer);
+
+        }
+    }
 }
---
+++ src/main/java/com/voluteamhub/backend/validator/LoginValidator.java
@@ -0,0 +1,12 @@
+package com.voluteamhub.backend.validator;
+
+import com.voluteamhub.backend.model.Volunteer;
+
+public class LoginValidator {
+
+    private Volunteer volunteer;
+
+    public LoginValidator(Volunteer volunteer1) {
+        volunteer = volunteer1;
+    }
+}
