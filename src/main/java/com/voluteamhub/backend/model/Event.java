7a50d5c77673f1f2bebb6657d74ebd0e4befce47
Lapadat Marius-Cristian <lapadatmarius23@gmail.com>
2023-11-13 17:29:11 +0200

    Added event class, eventRepository

---
+++ src/main/java/com/voluteamhub/backend/model/Event.java
@@ -0,0 +1,69 @@
+package com.voluteamhub.backend.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+public class Event {
+
+    @Id
+    @GeneratedValue
+    private long id;
+    private String name;
+    private String description;
+    private String photoUrl;
+    private LocalDate eventDate; /*aaaa-ll-zz*/
+    private List<Volunteer> volunteerList;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPhotoUrl() {
+        return photoUrl;
+    }
+
+    public void setPhotoUrl(String photoUrl) {
+        this.photoUrl = photoUrl;
+    }
+
+    public LocalDate getEventDate() {
+        return eventDate;
+    }
+
+    public void setEventDate(LocalDate eventDate) {
+        this.eventDate = eventDate;
+    }
+
+    public List<Volunteer> getVolunteerList() {
+        return volunteerList;
+    }
+
+    public void setVolunteerList(List<Volunteer> volunteerList) {
+        this.volunteerList = volunteerList;
+    }
+}
